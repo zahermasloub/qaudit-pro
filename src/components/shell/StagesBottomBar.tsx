a25ec80747d3376cf05bca5da8cdbd884d21b926
Zaher Mohamed Masloub <crc.qa2222@gmail.com>
2025-10-24 06:53:41 +0300

    FIX

--- src/components/shell/StagesBottomBar.tsx
+++ src/components/shell/StagesBottomBar.tsx
@@ -1,22 +1,22 @@
 'use client';
 
-import * as Drawer from '@radix-ui/react-drawer';
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function StagesBottomBar() {
   return (
     <div className="fixed inset-x-0 bottom-0 bg-white shadow lg:hidden z-40">
       <div className="flex gap-2 overflow-x-auto p-2">
-        <Drawer.Root>
-          <Drawer.Trigger className="px-3 py-1.5 rounded-lg bg-slate-100">
+        <Dialog.Root>
+          <Dialog.Trigger className="px-3 py-1.5 rounded-lg bg-slate-100">
             تفاصيل المراحل
-          </Drawer.Trigger>
-          <Drawer.Portal>
-            <Drawer.Overlay className="fixed inset-0 bg-black/20" />
-            <Drawer.Content className="fixed inset-x-0 bottom-0 bg-white rounded-t-2xl p-4">
-              <Drawer.Close className="mt-2">إغلاق</Drawer.Close>
-            </Drawer.Content>
-          </Drawer.Portal>
-        </Drawer.Root>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay className="fixed inset-0 bg-black/20" />
+            <Dialog.Content className="fixed inset-x-0 bottom-0 bg-white rounded-t-2xl p-4">
+              <Dialog.Close className="mt-2">إغلاق</Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </div>
     </div>
   );
